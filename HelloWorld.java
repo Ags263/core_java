6134d9abd950dde84b1e607335e96a8f382ddd6e
Ashish Behera <141529216+Ags263@users.noreply.github.com>
2024-10-09 12:54:06 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(" Hello Ashish");
+		System.out.println(num);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ datatypes.java
@@ -0,0 +1,47 @@
+package sample;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte var1 = 127;
+
+        // 2. short (2 bytes)
+        short var2 = 32000;
+
+        // 3. int (4 bytes)
+        int var3 = 100000;
+
+        // 4. long (8 bytes)
+        long var4  = 10000000000L;
+
+        // 5. char (2 bytes)
+        char var5 = 'A';
+
+        // 6. float (4 bytes)
+        float var6 = 3.145f;
+
+        // 7. double (8 bytes)
+        double var7 = 3.14159265359;
+
+        // 8. boolean (1 bit)
+        boolean var8 = false;
+
+        // Non-Primitive Data Types
+
+        // 1. String (non-primitive but often treated as a special class)
+        String str = "Ashish";
+        
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
+        System.out.println(var7);
+        System.out.println(var8);
+        System.out.println(str);
+	}
+
+}
