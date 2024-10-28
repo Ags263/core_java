4ce53237deea2529b51439de6ff9ec3fa2f97361
Ashish Behera <141529216+Ags263@users.noreply.github.com>
2024-10-28 13:10:45 +0530

    Add files via upload

---
+++ squareandperimeter.java
@@ -0,0 +1,19 @@
+package ArrayDemo;;
+
+public class squareandperimeter {
+
+	public static void main(String[] args) {
+/*
+Write a Program to Calculate Area and Perimeter of a square.
+
+
+create an Employee  class and display 5 Employee details with name,salary and age*/
+		int a = 20;
+		int square = a*a;
+		int perimeter = 4*a;
+		System.out.println("Square of a is :"+square);
+		System.out.println("Square of a is :"+perimeter);
+
+	}
+
+}
